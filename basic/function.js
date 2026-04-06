99af97ed26a365d2b0585872219fb4ef8366e800
siyalamna <132810681+siyalamna@users.noreply.github.com>
2026-04-06 16:43:24 +0000

    functions

---
+++ basic/function.js
@@ -0,0 +1,50 @@
+function sayMyName(){
+    console.log("A");
+    console.log("M");
+    console.log("N");
+    console.log("A");
+}
+
+// sayMyName();
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+const result = addition(2, 4);
+// console.log("Result is: ", result)
+
+function loginUser(usernanme = "Amna"){
+    if(!usernanme){
+        console.log("Please enter a username")
+        return
+    }
+    return `${usernanme} Just logged in`
+}
+
+// console.log(loginUser("GM"));
+
+const user = {
+    userName: "Amna",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`)
+}
+
+// handleObject(user);
+
+const myNewarray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewarray));
+
+function calculateCartPrice(... num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 400))
