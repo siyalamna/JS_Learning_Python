0246a74c194bcf4096ce4f3ac9f849584ac2cbca
siyalamna <132810681+siyalamna@users.noreply.github.com>
2026-04-06 07:34:43 +0000

    commit the changes

---
+++ basic/array.js
@@ -0,0 +1,39 @@
+// Array
+const myArr = [1, 2, 3, 4, 5]
+// console.log(myArr);
+
+// console.log(myArr[2]);
+
+const nameArr = ["Amna", "Asma", "Mariyam"]
+// console.log(nameArr[2]);
+
+// const newArr = new Array(1, 2, 3, 4)
+
+// Array methods()
+
+// myArr.push(6);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(2));
+
+let myArr1 = myArr.join();
+// console.log(typeof(myArr1));
+
+// console.log(myArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log(myn2);
+
---
+++ basic/date.js
@@ -0,0 +1,24 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+// let creatmyDate = new Date(2023, 0, 12);
+// let creatmyDate = new Date(2026, 3, 4, 5, 0)
+let creatmyDate = new Date("2026-01-12")
+// console.log(creatmyDate.toLocaleString()); 
+// console.log(creatmyDate.getTime());
+
+let myTimetsamp = Date.now();
+// console.log(myTimetsamp);
+// console.log(Date.now());
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate.getMonth());
